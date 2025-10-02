b1ee7f8f429a03a5357df3a4cd052c1a136dc53e
unknown <bluecrystal1811@gmail.com>
2025-10-02 21:12:00 +0700

    Nop bai

---
+++ LAB_3/Lab3.sql
@@ -0,0 +1,123 @@
+﻿-- Bài 1:
+
+-- Tổng giờ làm việc theo đề án
+
+-- Cách 1: CAST
+SELECT DA.TENDA,
+       CAST(SUM(PC.THOIGIAN) AS DECIMAL(10,2)) AS TongGio_DEC,
+       CAST(SUM(PC.THOIGIAN) AS VARCHAR(20))   AS TongGio_VARCHAR
+FROM DeAn DA
+JOIN PhanCong PC ON DA.MADA = PC.MADA
+GROUP BY DA.TENDA;
+
+-- Cách 2: CONVERT
+SELECT DA.TENDA,
+       CONVERT(DECIMAL(10,2), SUM(PC.THOIGIAN)) AS TongGio_DEC,
+       CONVERT(VARCHAR(20), SUM(PC.THOIGIAN))   AS TongGio_VARCHAR
+FROM DeAn DA
+JOIN PhanCong PC ON DA.MADA = PC.MADA
+GROUP BY DA.TENDA;
+
+-- Lương trung bình theo phòng ban
+-- CAST
+SELECT PB.TENPHG,
+       CAST(AVG(NV.LUONG) AS DECIMAL(10,2)) AS LuongTB_DEC,
+       REPLACE(CONVERT(VARCHAR,CAST(AVG(NV.LUONG) AS MONEY),1),'.00','') AS LuongTB_VARCHAR
+FROM PhongBan PB
+JOIN NhanVien NV ON PB.MAPHG = NV.PHG
+GROUP BY PB.TENPHG;
+
+-- CONVERT
+SELECT PB.TENPHG,
+       CONVERT(DECIMAL(10,2), AVG(NV.LUONG)) AS LuongTB_DEC,
+       REPLACE(CONVERT(VARCHAR,CONVERT(MONEY,AVG(NV.LUONG)),1),'.00','') AS LuongTB_VARCHAR
+FROM PhongBan PB
+JOIN NhanVien NV ON PB.MAPHG = NV.PHG
+GROUP BY PB.TENPHG;
+
+--Bài 2:
+
+--1. Tổng giờ làm việc theo đề án (CEILING, FLOOR, ROUND)
+
+SELECT DA.TENDA,
+       CEILING(SUM(PC.THOIGIAN)) AS Gio_CEILING,
+       FLOOR(SUM(PC.THOIGIAN))   AS Gio_FLOOR,
+       ROUND(SUM(PC.THOIGIAN),2) AS Gio_ROUND2
+FROM DeAn DA
+JOIN PhanCong PC ON DA.MADA = PC.MADA
+GROUP BY DA.TENDA;
+
+--2. Nhân viên có lương > lương TB phòng “Nghiên cứu” (làm tròn 2 số)
+
+SELECT NV.HONV, NV.TENLOT, NV.TENNV, NV.LUONG
+FROM NhanVien NV
+WHERE NV.LUONG >
+      (SELECT ROUND(AVG(NV2.LUONG),2)
+       FROM NhanVien NV2
+       JOIN PhongBan PB ON NV2.PHG = PB.MAPHG
+       WHERE PB.TENPHG = N'Nghiên cứu');
+
+--Bài 3:
+
+--1. Nhân viên có >2 thân nhân, định dạng theo yêu cầu
+
+SELECT 
+    UPPER(HONV)  AS HONV,                 -- họ in hoa
+    LOWER(TENLOT) AS TENLOT,              -- tên lót thường
+    STUFF(LOWER(TENNV),2,1,UPPER(SUBSTRING(TENNV,2,1))) AS TENNV, -- ký tự 2 in hoa
+    -- chỉ lấy tên đường: bỏ số nhà và phần sau dấu phẩy
+    LTRIM(
+        SUBSTRING(
+            DCHI,
+            PATINDEX('%[^0-9 ]%', DCHI),                           -- vị trí ký tự đầu tiên KHÔNG phải số/space
+            CASE WHEN CHARINDEX(',', DCHI) > 0                      -- nếu có dấu phẩy
+                 THEN CHARINDEX(',', DCHI) - PATINDEX('%[^0-9 ]%', DCHI)
+                 ELSE LEN(DCHI) - PATINDEX('%[^0-9 ]%', DCHI) + 1
+            END
+        )
+    ) AS Duong
+FROM dbo.NhanVien NV
+WHERE (
+        SELECT COUNT(*) 
+        FROM dbo.ThanNhan TN 
+        WHERE TN.MA_NVIEN = NV.MANV
+      ) > 2;
+
+--2. Phòng ban đông nhân viên nhất, thay tên trưởng phòng = 'Fpoly'
+
+SELECT TOP 1
+       PB.TENPHG,
+       'Fpoly' AS TruongPhong
+FROM PhongBan PB
+JOIN NhanVien NV ON PB.MAPHG = NV.PHG
+GROUP BY PB.TENPHG
+ORDER BY COUNT(NV.MANV) DESC;
+
+--Bài 4: 
+
+--1.Nhân viên có năm sinh từ 1960 đến 1965
+SELECT HONV, TENLOT, TENNV, NGSINH
+FROM dbo.NhanVien
+WHERE YEAR(NGSINH) BETWEEN 1960 AND 1965;
+
+--2.Tính tuổi hiện tại của mỗi nhân viên
+SELECT HONV, TENLOT, TENNV,
+       DATEDIFF(YEAR, NGSINH, GETDATE()) AS TUOI
+FROM dbo.NhanVien;
+
+--3.Cho biết thứ trong tuần mà nhân viên sinh
+SELECT HONV, TENLOT, TENNV,
+       DATENAME(WEEKDAY, NGSINH) AS ThuSinh
+FROM dbo.NhanVien;
+
+--4. Thống kê số lượng nhân viên, tên trưởng phòng, ngày nhận chức trưởng phòng
+SELECT 
+    PB.TENPHG,
+    NV.HONV + ' ' + NV.TENLOT + ' ' + NV.TENNV AS TruongPhong,
+    COUNT(NV2.MANV) AS SoLuongNV,
+    FORMAT(PB.NG_NHANCHUC, 'dd-MM-yy') AS NgayNhanChuc
+FROM dbo.PhongBan PB
+LEFT JOIN dbo.NhanVien NV  ON PB.TRPHG = NV.MANV      -- trưởng phòng
+LEFT JOIN dbo.NhanVien NV2 ON PB.MAPHG = NV2.PHG      -- nhân viên phòng
+GROUP BY PB.TENPHG, NV.HONV, NV.TENLOT, NV.TENNV, PB.NG_NHANCHUC;
+
---
+++ LAB_4/Lab4.sql
@@ -0,0 +1,101 @@
+﻿--Bài 1:
+
+--Sử dụng CASE và IF…ELSE trong cơ sở dữ liệu QLDA.
+
+-- Hiển thị TenNV (thêm tiền tố Mr/Ms tùy giới tính)
+SELECT 
+    CASE 
+        WHEN PHAI = N'Nữ' THEN 'Ms. ' + TENNV
+        ELSE 'Mr. ' + TENNV
+    END AS TenNV
+FROM NHANVIEN;
+
+--Tính thuế theo lương
+SELECT 
+    TENNV,
+    LUONG,
+    CASE 
+        WHEN LUONG < 25000 THEN LUONG*0.1
+        WHEN LUONG < 30000 THEN LUONG*0.12
+        WHEN LUONG < 40000 THEN LUONG*0.15
+        WHEN LUONG < 50000 THEN LUONG*0.20
+        ELSE LUONG*0.25
+    END AS Thue
+FROM NHANVIEN;
+
+--Tăng lương và phân loại
+SELECT 
+    TENNV,
+    CASE 
+        WHEN LUONG < (SELECT AVG(LUONG) 
+                      FROM NHANVIEN B 
+                      WHERE B.PHG = A.PHG)
+             THEN 'TangLuong'
+        ELSE 'KhongTangLuong'
+    END AS TinhTrang,
+    CASE 
+        WHEN LUONG < (SELECT AVG(LUONG) 
+                      FROM NHANVIEN B 
+                      WHERE B.PHG = A.PHG)
+             THEN 'NhanVien'
+        ELSE 'TruongPhong'
+    END AS XepLoai
+FROM NHANVIEN A;
+
+--Bài 2:
+
+--Dùng vòng lặp (WHILE) để liệt kê nhân viên có MaNV là số chẵn.
+
+--Tất cả MaNV chẵn
+DECLARE @id INT = 1;
+WHILE @id <= (SELECT MAX(CAST(MANV AS INT)) FROM NHANVIEN)
+BEGIN
+    IF @id % 2 = 0
+        SELECT HONV, TENLOT, TENNV, MANV
+        FROM NHANVIEN
+        WHERE MANV = RIGHT('000' + CAST(@id AS VARCHAR),3);
+    SET @id += 1;
+END;
+
+--Chẵn nhưng bỏ MaNV = 4
+DECLARE @id INT = 1;
+WHILE @id <= (SELECT MAX(CAST(MANV AS INT)) FROM NHANVIEN)
+BEGIN
+    IF @id % 2 = 0 AND @id <> 4
+        SELECT HONV, TENLOT, TENNV, MANV
+        FROM NHANVIEN
+        WHERE MANV = RIGHT('000' + CAST(@id AS VARCHAR),3);
+    SET @id += 1;
+END;
+
+--Bài 3:
+
+--Quản lý lỗi với TRY…CATCH và RAISERROR.
+
+--Thêm dữ liệu thành công và thất bại
+BEGIN TRY
+    INSERT INTO PHONGBAN(TENPHG, MAPHG) VALUES(N'Kinh Doanh', '9');
+    PRINT N'Thêm dữ liệu thành công';
+END TRY
+BEGIN CATCH
+    PRINT N'Thêm dữ liệu thất bại';
+END CATCH;
+
+--Chèn sai dữ liệu
+BEGIN TRY
+    INSERT INTO PHONGBAN(MAPHG, TENPHG) VALUES('P09', N'Kinh Doanh');
+    PRINT N'Thêm dữ liệu thành công';
+END TRY
+BEGIN CATCH
+    PRINT N'Thêm dữ liệu thất bại';
+END CATCH;
+
+--Chia cho 0 và báo lỗi
+DECLARE @chia INT = 10;
+BEGIN TRY
+    SELECT @chia / 0;
+END TRY
+BEGIN CATCH
+    RAISERROR (N'Lỗi chia cho 0',16,1);
+END CATCH;
+
